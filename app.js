359e1fcdf0f8de041e8fe9217f1e73ed735bed6e
subhashchandra01 <subhashaj05@gmail.com>
2025-02-18 04:54:37 +0000

    Fullstack Print part2 changes made

--- app.js
+++ app.js
@@ -20,5 +20,7 @@ app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
 // Boot the server
-app.listen(port, () => console.log(`Server listening on port ${port}`))
 
+app.get('/orders', api.listOrders)
+app.get('/orders', api.createOrder)
+app.listen(port, () => console.log(`Server listening on port ${port}`))
